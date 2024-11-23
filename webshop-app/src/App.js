c975f3d93b333124d8baa4a81a4d12f932222c9a
emilijadunoska <emilijadunoska@gmail.com>
2024-11-23 23:10:54 +0200

    Initialize Hotjar script

--- webshop-app/src/App.js
+++ webshop-app/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import Hotjar from "@hotjar/browser";
 
 import BookstoreRoutes from "./components/BookstoreRoutes";
 
 function App() {
+  const siteId = 5218732;
+  const hotjarVersion = 6;
+
+  Hotjar.init(siteId, hotjarVersion);
+
   return (
     <BrowserRouter>
       <BookstoreRoutes />
